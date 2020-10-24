d1ab16a54f61cdc554a82d3ae274b9266279892c
Elvis Oric <elvis.oric90@gmail.com>
2020-10-24 23:12:32 +0200

    Initial commit.

---
+++ include/foo.hpp
@@ -0,0 +1,6 @@
+#ifndef MESON_CONAN_FOO_HPP
+#define MESON_CONAN_FOO_HPP
+
+void foo();
+
+#endif // MESON_CONAN_FOO_HPP
---
+++ srcs/foo.cpp
@@ -0,0 +1,4 @@
+#include <foo.hpp>
+#include <fmt/core.h>
+
+void foo() { fmt::print("Hello Foo !\n"); }
